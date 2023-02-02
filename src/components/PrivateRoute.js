3209fdc0e526af8115ca8b5502ed3ac4e1da27dc
Shubhamraut9497 <114891419+Shubhamraut9497@users.noreply.github.com>
2023-02-02 10:53:27 +0530

    commit

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 
+const PrivateRoute=(props)=>{
+    const navigate=useNavigate();
+   if(!window.localStorage.getItem("username") && !window.localStorage.getItem("password")){
+     navigate('/login')
+   }
+   else{
+    
+    return (
+        <>{props.children}</>
+    )
+   }
+}
+export default PrivateRoute;
